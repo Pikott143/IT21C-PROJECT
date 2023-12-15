c4bb5a0e72255c7cbdeb53d53f7408987b2758d6
Pikott143 <Theunseenmaiden@gmail.com>
2023-12-16 01:34:22 +0800

    na bilin sa linya

--- src/person/instructor.java
+++ src/person/instructor.java
@@ -1,13 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package person;
 
-/**
- *
- * @author Aj Cervantes
- */
-public class instructor {
+    class instructor extends person {
     
-}
+        private double salary;
+    
+        public instructor(String name, int yearOfBirth, int salary) {
+            super(name, yearOfBirth);
+            this.salary = salary;
+        }
+    
+        // Getter and setter methods for salary
+        public double getSalary() {
+            return salary;
+        }
+    
+        public void setSalary(double salary) {
+            this.salary = salary;
+        }
+    
+        // toString method
+        public String toString() {
+            return "Instructor [name=" + getName() + ", yearOfBirth=" + getYearOfBirth() + ", salary=" + salary + "]";
+        }
+
+    }
+
+
+     
+
