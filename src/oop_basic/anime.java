88edafc355ba66c575e274191ca0571b8afb093e
Pikott143 <Theumseenmaide@gmail.com>
2023-12-16 01:15:51 +0800

    seventh

---
+++ src/oop_basic/anime.java
@@ -0,0 +1,29 @@
+package oop_basic;
+
+public class anime {
+    
+     String anime_collection;
+ double merchandise_price;
+ 
+ 
+anime(String drug_name,double drug_price){
+
+this.anime_collection = anime_collection;
+this.merchandise_price = merchandise_price;
+
+}
+
+void showAnimeCollection(){
+System.out.println(anime_collection);
+
+}
+
+void showMerchandisePrice(){
+System.out.println(merchandise_price);
+
+
+}
+       
+}
+
+
--- src/oop_basic/main.java
+++ src/oop_basic/main.java
@@ -1,13 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package oop_basic;
 
-/**
- *
- * @author Aj Cervantes
- */
 public class main {
-    
+       public static void main(String[] args) {
+ 
+     anime collection1 = new anime("one piece:",300.00);
+     
+     anime collection2 = new anime("jujutsu kaisen:",250.00);
+     
+     anime collection3 = new anime("iruma-kun:",200.00);
+     
+     anime collection4 = new anime("dr.stone:",300.00);
+     
+     collection1. showAnimeCollection();   
+     collection1.showMerchandisePrice();
+     
+     collection2. showAnimeCollection();   
+     collection2.showMerchandisePrice();
+     
+     collection3. showAnimeCollection();   
+     collection3.showMerchandisePrice();
+     
+     collection4. showAnimeCollection();   
+     collection4.showMerchandisePrice();
+    }
 }
