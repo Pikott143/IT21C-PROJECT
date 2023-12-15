11f10d3997551f084f6f3a1ccc13d13607b37e9c
Pikott143 <Theunseenmaiden@gmail.com>
2023-12-16 00:23:54 +0800

    third commit

--- src/bank/account.java
+++ src/bank/account.java
@@ -1,13 +1,57 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package bank;
 
 /**
  *
  * @author Aj Cervantes
  */
-public class account {
+
+class account {
+   
+        private String name;
+        private int age;
+        private int accountNumber;
+        private double balance;
+       
     
-}
+        public String getName(){
+            return name;
+        }
+        public void setName(String name){
+            this.name = name;
+        }
+        public int getAge(){
+            return age;
+        }
+        public void setAge(int age){
+            this.age = age;
+        }
+        public int getAccountnumber(){
+            return accountNumber;
+        }
+        public void setAccountNumber(int accountNumber){
+            this.accountNumber = accountNumber;
+        }
+        public double getBalance(){
+            return balance;
+        }
+        public void setBalance(double amount){
+            this.balance = 0.0;
+        }
+        public void  deposit(double amount){
+            if(amount > 0){
+                balance += amount;
+            System.out.println("Deposited P" + amount);
+        }else{
+        System.out.println("Invalid deposit amount");
+        }
+    }
+        public void displaySummary(){
+         System.out.println("\nAccount Summary:");
+            System.out.println("Name: " + name);
+            System.out.println("Age: " + age);
+            System.out.println("AccountNumber: " + accountNumber);
+            System.out.println("Balance: P" + balance);
+    
+        }
+    }
+
--- src/bank/main.java
+++ src/bank/main.java
@@ -1,13 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package bank;
 
 /**
  *
  * @author Aj Cervantes
  */
-public class main {
-    
+import bank.account;
+
+class main {
+    public static void main(String[] args){
+                
+        account account = new account();
+        
+        account.setName("Annalyn Jean Maraviles Cervantes");
+        account.setAge(19);
+        account.setAccountNumber(20221154);
+        account.deposit(90765354);
+        account.deposit(87687766);
+        account.displaySummary();
+        
+    }
 }
